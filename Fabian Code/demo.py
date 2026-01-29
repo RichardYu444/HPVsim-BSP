bd4f4550950be6f87b3cc662719023c7c045d4f6
RichardYu444 <richard.yu004@gmail.com>
2026-01-29 10:35:18 +0000

    Renaming/restrucutring

--- Fabian Code/demo.py
+++
@@ -1,8 +0,0 @@
-
-
-import NHS_2025_lambdamu, NHS_Vacc
-from basePars import base_pars
-import hpvsim as hpv
-
-
-calib.df.to_csv(f"{name}.csv")
---
+++ FabianCode/demo.py
@@ -0,0 +1,8 @@
+
+
+import NHS_2025_lambdamu, NHS_Vacc
+from basePars import base_pars
+import hpvsim as hpv
+
+
+calib.df.to_csv(f"{name}.csv")
